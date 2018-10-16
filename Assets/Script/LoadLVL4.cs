ae1a1b6d57f2a56e0e738ab87d21a2fff7b87552
John Kaser <20.john.kaser@ksd.org>
2018-10-16 13:50:58 -0700

    nearly there now m8

---
+++ Assets/Script/LoadLVL4.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLVL4 : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "player")
+        {
+            // attach to the object ending the game, not the player
+            SceneManager.LoadScene("Level4");
+        }
+
+    }
+
+}
