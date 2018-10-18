c53fc5f2ba36970f5bf31ecc2dee29bc39f7dcfa
John Kaser <20.john.kaser@ksd.org>
2018-10-18 11:44:50 -0700

    cannon sound

--- Assets/Script/Shoot.cs
+++ Assets/Script/Shoot.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Shoot : MonoBehaviour {
+    public AudioClip soundToPlay;
     public float bulletLife;
     public float bulletSpeed;
     public float fireSpeed;
@@ -20,6 +21,7 @@ public class Shoot : MonoBehaviour {
         if (Input.GetButton("Fire1") && timer > fireSpeed)
         {
             timer = 0;
+            
             Vector3 mousePosition = Input.mousePosition;
             mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
             mousePosition.z = 0;
@@ -30,6 +32,7 @@ public class Shoot : MonoBehaviour {
             GameObject bullet = (GameObject)Instantiate(prefab, transform.position, Quaternion.identity);
             bullet.GetComponent<Rigidbody2D>().velocity = shootDir.normalized * bulletSpeed;
             Destroy(bullet, bulletLife);
+            Camera.main.GetComponent<AudioSource>().PlayOneShot(soundToPlay);
 
         }
 	}
